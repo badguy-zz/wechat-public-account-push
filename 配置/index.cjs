ffb4f4d095bc072f83d1269c381b1ab403645e65
badguy-zz <112557223+badguy-zz@users.noreply.github.com>
2023-04-29 00:09:00 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -1,11 +1,10 @@
 /* eslint-disable */
-
 /**
  * 此项目配置为方便新人使用，已缩减至最简配置。
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {
+export const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx299f6770beae766f',
@@ -40,12 +39,12 @@ const USER_CONFIG = {
         },
       ],
       // 我们在一起已经有xxxx天了的配置
-//       customizedDateList: [
-//         // 在一起的日子
-//         { keyword: 'love_day', date: '2022-09-08' },
-//         // 结婚纪念日
-//         { keyword: 'marry_day', date: '2022-09-09' },
-//       ],
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
     },
   ],
 
